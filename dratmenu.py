d299ddf2963f7dabd952f9ed1ac575cd6f214d9f
Daniel Maturana <dimatura@cmu.edu>
2013-09-09 18:40:45 -0400

    stylistic changes

--- dratmenu.py
+++ dratmenu.py
@@ -2,20 +2,35 @@
 
 import subprocess
 
+# this is the separator string. if one of your windows has this in its name
+# things will break
 sep = '^[^[^['
 rp_cmd = ['ratpoison', '-c', "windows " + sep.join(['%n','%c','%t'])]
 rp = subprocess.Popen(rp_cmd, stdout=subprocess.PIPE)
 windows = [ln.split(sep) for ln in rp.stdout.read().strip().split('\n')]
 rp.wait()
-windows_txt = '\n'.join([ ' '.join([ln[0].rjust(3), ln[1].ljust(10)[:10], ln[2]]) for ln in windows ])
+# change this for different menu strings
+windows_txt = '\n'.join([ ' '.join([ln[0].rjust(3), ln[1].ljust(10)[:10], ln[2]])
+    for ln in windows ])
 
+# some font names candidates
 #font = "-*-clean-*-r-*-*-*-*-*-*-*-*-*-*"
 #font = '-*-clean-*-r-*-*-17-*-*-*-*-*-*-*'
 #font = '-*-helvetica-*-r-*-*-17-*-*-*-*-*-*-*'
+
 font = '-*-terminus-*-r-*-*-14-*-*-*-*-*-*-*'
-dmenu_cmd = ['dmenu', '-i', '-sb', '#859900', '-nb', '#002b36', '-fn', font, '-l', '20']
-print dmenu_cmd
-dmenu = subprocess.Popen(dmenu_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+normal_bg_color = '#002b36'
+selected_bg_color = '#859900'
+num_lines_vert = '20'
+
+dmenu_cmd = ['dmenu', '-i',
+            '-sb', selected_bg_color,
+            '-nb', normal_bg_color,
+            '-fn', font,
+            '-l', num_lines_vert]
+dmenu = subprocess.Popen(dmenu_cmd,
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE)
 selection = dmenu.communicate(windows_txt)[0]
 sel_number = selection[:3].strip()
 
